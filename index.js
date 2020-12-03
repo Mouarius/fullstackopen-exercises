87749145f1520c071d6db64a7a5db17ec67d0db5
mouarius <mouarius@gmail.com>
2020-12-04 00:51:54 +0100

    Added morgan request logger

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 
+var morgan = require('morgan')
+
+morgan.token("content", (req)=>{
+    return JSON.stringify(req.body)
+})
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
     {
