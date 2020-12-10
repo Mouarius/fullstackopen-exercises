713ba7d9329e7c7e5472e9f244b01c765e82eb7a
mouarius <mouarius@gmail.com>
2020-12-10 14:20:16 +0100

    backend linked to mongodb

--- index.js
+++ index.js
@@ -1,111 +1,88 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+const Person = require('./models/person')
 
-var morgan = require("morgan");
+morgan.token('content', (req) => {
+	return JSON.stringify(req.body)
+})
 
-morgan.token("content", (req) => {
-  return JSON.stringify(req.body);
-});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-app.use(cors());
-app.use(express.static("build"));
+app.use(cors())
 
-app.use(express.json());
-app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms :content"
-  )
-);
+app.use(express.static('build'))
+app.use(express.json())
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-  {
-    name: "Alice Capdemourlin",
-    number: "33-23-6423122",
-    id: 5,
-  },
-];
+app.get('/', (req, res) => {
+	res.send('App is working')
+})
 
-app.get("/", (req, res) => {
-  res.send("App is working");
-});
-
-app.get("/api/persons", (req, res) => {
-  res.status(200).json(persons);
-});
+app.get('/api/persons', (req, res) => {
+	Person.find({}).then((persons) => {
+		res.status(200).json(persons)
+	})
+})
 
 app.get(`/info`, (req, res) => {
-  const reqDate = new Date();
-  res
-    .status(200)
-    .send(
-      `Phonebook has info for ${
-        persons.length
-      } people <br/><br/> ${reqDate.toUTCString()}`
-    );
-});
+	const reqDate = new Date()
+	Person.find({}).then((persons) => {
+		res.status(200).send(`Phonebook has info for ${persons.length} people <br/><br/> ${reqDate.toUTCString()}`)
+	})
+})
 
 app.get(`/api/persons/:id`, (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    res.status(200).json(person);
-  } else {
-    res.status(404).end();
-  }
-});
+	const id = req.params.id
+	Person.findById(id)
+		.then((person) => {
+			res.status(200).json(person)
+		})
+		.catch((error) => {
+			res.status(404).json({ error: error })
+		})
+})
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
-});
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	persons = persons.filter((person) => person.id !== id)
+	res.status(204).end()
+})
 
-app.post("/api/persons", (req, res) => {
-  const id = Math.floor(Math.random() * 10000);
-  const person = req.body;
-  console.log("person :>> ", person);
-  if (person.number) {
-    if (person.name) {
-      if (
-        persons.find((p) => p.name.toLowerCase() === person.name.toLowerCase())
-      ) {
-        res.status(409).json({ error: `The name must be unique.` });
-      } else {
-        person.id = id;
-        console.log("person :>> ", person);
-        persons = persons.concat(person);
-        res.status(201).json(persons);
-      }
-    } else {
-      res.status(400).json({ error: "No name has been given." });
-    }
-  } else {
-    res.status(400).json({ error: "No number has been given." });
-  }
-});
+app.post('/api/persons', (req, res) => {
+	const body = req.body
+	Person.find({}).then((persons) => {
+		console.log('body :>> ', body)
+		console.log('persons :>> ', persons)
+		if (body.number) {
+			if (body.name) {
+				if (persons.find((p) => p.name.toLowerCase() === body.name.toLowerCase())) {
+					res.status(409).json({ error: `The name must be unique.` })
+				} else {
+					var person = new Person({
+						name: body.name,
+						number: body.number,
+					})
+					person
+						.save()
+						.then((savedPerson) => {
+							res.status(201).json(savedPerson)
+						})
+						.catch((error) => {
+							res.status(400).json({ error: error })
+						})
+				}
+			} else {
+				res.status(400).json({ error: 'No name has been given.' })
+			}
+		} else {
+			res.status(400).json({ error: 'No number has been given.' })
+		}
+	})
+})
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}/`);
-});
+	console.log(`Server running on http://localhost:${PORT}/`)
+})
