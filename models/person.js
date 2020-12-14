45c33c618d3fd91190c587cef258ec71dafab611
mouarius <mouarius@gmail.com>
2020-12-14 01:19:06 +0100

    removed unused

--- models/person.js
+++ models/person.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
-const opts = {runValidators: true}
-
-//console.log(`connecting to : `, url)
 
 mongoose
 	.connect(url, {
