c7eb46560cfd9b17a44459a95c6abcf8b4d384ea
mouarius <mouarius@gmail.com>
2021-02-13 00:22:41 +0100

    Exercise 9.5

--- part9/first-steps/index.ts
+++ part9/first-steps/index.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator";
 const app = express();
 
-const PORT: number = 8000;
+const PORT: number = 3002;
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack !");
+});
+
+app.get("/bmi/", (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+  if (height && weight) {
+    try {
+      const bmi = calculateBmi(height, weight);
+      res.send({ weight, height, bmi });
+    } catch (e) {
+      res.send({ error: e.message });
+    }
+  } else {
+    res.send({ error: "malformatted parameters" });
+  }
 });
 
 app.listen(PORT, () => {
